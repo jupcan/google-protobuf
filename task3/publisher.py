2eb0351e94cf8ead3749aef84658f86d0312fa76
jupcan <jpereac@gmail.com>
2018-12-12 10:46:26 +0100

    task3 done

---
+++ task3/publisher.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+# -*- coding: utf-8; mode: python -*-
+import json
+import time
+import random
+import paho.mqtt.client as mqtt
+
+def callback(client, userdata, message):
+    token = message.payload.decode()
+    data = {
+        'token': token,
+        'fullname': 'Juan Perea Campos',
+        'identifier': '71229899A',
+        'grade': 'PASS',
+    }
+    publisher = mqtt.Client()
+    publisher.connect('161.67.33.218')
+    while 1:
+        publisher.publish('students', json.dumps(data))
+        time.sleep(1)
+
+subscriber = mqtt.Client()
+subscriber.on_message = callback
+subscriber.connect('161.67.33.218')
+subscriber.subscribe('tokens')
+subscriber.loop_forever()
