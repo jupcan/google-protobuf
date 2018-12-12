362c4903c356f0b35236e69ac725a4df4ad263df
jupcan <jpereac@gmail.com>
2018-12-12 10:58:19 +0100

    localhost ex

---
+++ task3/localhost/publisher.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+# -*- coding: utf-8; mode: python -*-
+import json
+import time
+import random
+import paho.mqtt.client as mqtt
+
+def take_reading():
+    return {
+        'identifier': 'X002',
+        'value': random.randint(20, 40),
+        'unit': 'Celsius',
+        'timestamp': time.time(),
+    }
+
+publisher = mqtt.Client()
+publisher.connect('localhost')
+
+while 1:
+    publisher.publish('temperature/X002', json.dumps(take_reading()))
+    time.sleep(1)
